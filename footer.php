5fb1fdd60305460ae66cb77d921ade69e82e5ad6
Tim Shedor <tshedor@gmail.com>
2015-05-11 02:01:03 -0700

    add footer and dropdown styles

--- footer.php
+++ footer.php
@@ -1,15 +1,22 @@
 <?php wp_footer(); ?>
-<div class="footer">
+<footer class="footer">
 	<div class="row clearfix">
-		<div class="large-12 columns text-center">
-			<div class="social-icons">
-				<?php Traction::social_header(); ?>
-			</div>
+		<div class="large-12 columns">
+			 <?php wp_nav_menu(array( 'theme_location' => 'footer_menu', 'container' => '', 'items_wrap' => '<ul class="link-list">%3$s</ul>', )); ?>
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="large-6 columns">
 			<div class="copyright">
 				&copy; Copyright <a href="<?php bloginfo('url') ?>" title="<?php bloginfo('name') ?>"><?php bloginfo('name') ?></a> <?php echo date('Y'); ?>
 			</div>
 		</div>
+		<div class="large-6 columns text-right">
+			<div class="social-icons">
+				<?php Traction::social_header(); ?>
+			</div>
+		</div>
 	</div>
-</div>
+</footer>
 </body>
 </html>
